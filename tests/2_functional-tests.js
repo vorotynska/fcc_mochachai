24461d57a4aec3e267810ea16425780445d198c2
Irina Carpincic <iryna.vorotynska@gmail.com>
2023-09-11 13:16:29 +0200

    first commit

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -16,8 +16,8 @@ suite('Functional Tests', function () {
         .keepOpen()
         .get('/hello')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
           done();
         });
     });
@@ -28,42 +28,62 @@ suite('Functional Tests', function () {
         .keepOpen()
         .get('/hello?name=xy_z')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello xy_z');
           done();
         });
     });
     // #3
-    test('Send {surname: "Colombo"}', function (done) {
+    test('send {surname: "Colombo"}', function (done) {
       chai
         .request(server)
         .keepOpen()
         .put('/travellers')
-
+        .send({
+          surname: "Colombo"
+        })
         .end(function (err, res) {
-          assert.fail();
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, "Cristoforo");
+          assert.equal(res.body.surname, "Colombo");
 
           done();
         });
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
-      done();
+      chai
+        .request(server)
+        .keepOpen()
+        .put('/travellers')
+        .send({
+          surname: "da Verrazzano"
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, "Giovanni");
+          assert.equal(res.body.surname, "da Verrazzano");
+          done();
+        })
     });
   });
 });
 
 const Browser = require('zombie');
-
+Browser.site = "https://boilerplate-mochachai.vorotynska.repl.co/"
 suite('Functional Tests with Zombie.js', function () {
+  const browser = new Browser();
+  suiteSetup(function (done) {
+    return browser.visit('/', done);
+  });
   this.timeout(5000);
 
 
 
   suite('Headless browser', function () {
-    test('should have a working "site" property', function() {
+    test('should have a working "site" property', function () {
       assert.isNotNull(browser.site);
     });
   });
@@ -71,15 +91,29 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
-    });
+      browser.fill('surname', 'Colombo').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Cristoforo');
+          browser.assert.text('span#surname', 'Colombo');
+          browser.assert.elements('span#dates', 1);
+          done();
+        });
+      });
+    })
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
+      browser.fill('surname', 'Vespucci').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Amerigo');
+          browser.assert.text('span#surname', 'Vespucci');
+          browser.assert.elements('span#dates', 1);
+          done();
+        });
+      });
 
-      done();
+
     });
   });
 });
